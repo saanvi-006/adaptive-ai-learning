5a22420e6fea8e1b9d4e8012ad7329ecbef535a0
Arpanjotkaur1 <arpnbk@gmail.com>
2026-04-14 22:56:53 +0530

    update

--- app/services/embeddings/embedder.py
+++ app/services/embeddings/embedder.py
@@ -1,9 +1,15 @@
 from sentence_transformers import SentenceTransformer
 
-model = SentenceTransformer("all-MiniLM-L6-v2")
+model = None
+
+def get_model():
+    global model
+    if model is None:
+        model = SentenceTransformer("all-MiniLM-L6-v2")
+    return model   # ✅ always return
 
 def embed_text(chunks):
-    return model.encode(chunks)
+    return get_model().encode(chunks)
 
 def embed_query(query: str):
-    return model.encode([query])[0]
+    return get_model().encode([query])[0]
