821556a00bcb3f56b016e02c4be431e4e4a937f2
Khushicodes15 <khushidhona06@gmail.com>
2026-04-14 12:21:52 +0530

    Added database url

--- app/api/db/database.py
+++ app/api/db/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql+asyncpg://postgres:password@localhost:5432/ai_learning"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 
