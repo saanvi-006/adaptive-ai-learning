6aa5ec9a3c7c25d8c50eceb54b4262f37b37b6eb
Khushicodes15 <khushidhona06@gmail.com>
2026-04-15 12:09:31 +0530

    fix:Render and local clash

--- app/api/db/database.py
+++ app/api/db/database.py
@@ -1,17 +1,23 @@
 import os
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
-
+if os.getenv("RENDER") is None:
+    from dotenv import load_dotenv
+    load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 if not DATABASE_URL:
     raise ValueError("DATABASE_URL not set")
 
-
-if DATABASE_URL.startswith("postgresql://"):
+# ✅ Handle BOTH formats safely (only once)
+if DATABASE_URL.startswith("postgres://"):
     DATABASE_URL = DATABASE_URL.replace(
-        "postgresql://", "postgresql+asyncpg://"
+        "postgres://", "postgresql+asyncpg://", 1
+    )
+elif DATABASE_URL.startswith("postgresql://"):
+    DATABASE_URL = DATABASE_URL.replace(
+        "postgresql://", "postgresql+asyncpg://", 1
     )
 
 engine = create_async_engine(
