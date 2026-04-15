17b775179245a27badbdcbbccdc191a476cac791
Khushicodes15 <khushidhona06@gmail.com>
2026-04-15 14:41:00 +0530

    trials

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from app.api.db.database import engine, Base
-# from app.api.routes import upload, query, explain, summarize
+from app.api.routes import upload, query, explain, summarize
 # from app.api.routes import documents, learning, tracking, system
 # from app.api.routes import flashcards
 
@@ -31,11 +31,11 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# app.include_router(upload.router, tags=["Documents"])
+app.include_router(upload.router, tags=["Documents"])
 # app.include_router(flashcards.router, tags=["Flashcards"])
-# app.include_router(query.router, tags=["AI Interaction"])
-# app.include_router(explain.router, tags=["AI Interaction"])
-# app.include_router(summarize.router, tags=["AI Interaction"])
+app.include_router(query.router, tags=["AI Interaction"])
+app.include_router(explain.router, tags=["AI Interaction"])
+app.include_router(summarize.router, tags=["AI Interaction"])
 # app.include_router(documents.router, tags=["Documents"])
 # app.include_router(learning.router, tags=["Quiz"])
 # app.include_router(tracking.router, tags=["Tracking"])
